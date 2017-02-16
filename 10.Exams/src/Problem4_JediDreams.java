e11eccaace779414f1b735a3af8ad3964bc111f3
inaabadjieva <ina_abadjieva@yahoo.com>
2017-02-16 13:49:59 +0200

    update

--- 10.Exams/src/Problem4_JediDreams.java
+++ 10.Exams/src/Problem4_JediDreams.java
@@ -2,56 +2,52 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class Problem4_JediDreams {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
 
-        TreeMap<String, ArrayList<String>> methods = new TreeMap<>();
-        Pattern patternStaticMethod = Pattern.compile("(?<=static)\\s+[a-zA-Z]+\\s+([a-zA-Z]+)\\s*\\(");
-        Pattern patternMethod = Pattern.compile("[.\\s]([A-Z]+[a-zA-Z]+?)\\s*\\(");
+        TreeMap<String, List<String>> methods = new TreeMap<>();
+        Pattern pattern = Pattern.compile("([a-zA-Z]*[A-Z]+[A-Za-z]*)\\s*\\(");
         String method = "";
         for (int i = 0; i < n; i++) {
             String line = reader.readLine();
-            Matcher matcherStaticMethod = patternStaticMethod.matcher(line);
-            if(matcherStaticMethod.find()){
-                method = matcherStaticMethod.group(1);
-                methods.put(method, new ArrayList<>());
-                continue;
-            }
+            Matcher matcher = pattern.matcher(line);
 
-            Matcher matcherMethod = patternMethod.matcher(line);
-            while(matcherMethod.find()){
-                methods.get(method).add(matcherMethod.group(1));
+            if (line.trim().startsWith("static")) {
+                if(matcher.find()) {
+                    method = matcher.group(1);
+                    methods.put(method, new ArrayList<>());
+                }
+            }
+            while (matcher.find()) {
+                methods.get(method).add(matcher.group(1));
             }
         }
 
         //sort and print
         methods.entrySet().stream()
-                .sorted((m1, m2) -> Integer.compare(m2.getValue().size(), m1.getValue().size()))
+                .sorted((a, b) -> Long.compare(b.getValue().size(), a.getValue().size()))
                 .forEach(m -> {
-                    StringBuilder result = new StringBuilder();
-                    result.append(m.getKey());
-                    if(m.getValue().size() > 0){
-                        result.append(" -> ").append(m.getValue().size()).append(" -> ");
+                    if (m.getValue().isEmpty()) {
+                        System.out.printf("%s -> None%n", m.getKey());
                     } else {
-                        result.append(" -> None");
+                        System.out.printf("%s -> %d -> %s%n",
+                            m.getKey(),
+                            m.getValue().size(),
+                            String.join(", ", m.getValue()
+                                .stream()
+                                .sorted(String::compareTo)
+                                .collect(Collectors.toList())));
                     }
 
-                    m.getValue().stream()
-                            .sorted((m1, m2) -> m1.compareTo(m2))
-                            .forEach(x -> result.append(x).append(", "));
+        });
 
-                    if(m.getValue().size() > 0){
-                        System.out.println(result.substring(0, result.length() - 2));
-                    } else {
-                        System.out.println(result);
-                    }
-
-                });
     }
 }
